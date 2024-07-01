34d8d33c46eb66723ad8e03d7539e1628e5cc027
siran-1 <siranjeevik21@gmail.com>
2024-07-01 18:13:25 +0100

    changes

--- src/components/QuickSortComponent.jsx
+++ src/components/QuickSortComponent.jsx
@@ -76,6 +76,8 @@ function QuickSortComponent() {
                 <div className="quicksort-visualizer">{quickSortRenderer(arr)}</div>
                 <div className="mt-1 quicksort-controls">
                     <button className="btn arr-btn-unshift" onClick={startSorting} disabled={isSorting}>Start &#10148;</button>
+                    <button className="btn arr-btn-shift" onClick={reset} disabled={isSorting}>Reset &#10148;</button>
+
                 </div>
             </div>
             <div className="quicksort-child-two">
